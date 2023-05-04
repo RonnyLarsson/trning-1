141c4b947de4f6b7ed2fdeb1d403c2de13f53596
RonnyLarsson <132570294+RonnyLarsson@users.noreply.github.com>
2023-05-04 13:17:41 +0200

    Initiala filer för MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,53 @@
+let Slummässigt = 0
+input.onGesture(Gesture.Shake, function () {
+    Slummässigt = randint(1, 6)
+    if (Slummässigt == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (Slummässigt == 2) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (Slummässigt == 3) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (Slummässigt == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (Slummässigt == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+})
